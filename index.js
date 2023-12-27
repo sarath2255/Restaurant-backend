0b15bd99499cbd8b8d507af0ab1b608cf1f898c8
sarath2255 <sarath2255z@gmail.com>
2023-12-27 14:02:51 +0530

    restaurant backend

---
+++ index.js
@@ -0,0 +1,26 @@
+//import json-server
+const jsonserver = require('json-server')
+
+// create a server application using json server
+
+const restserver = jsonserver.create()
+
+
+// setup path for db.json
+
+const router = jsonserver.router()
+
+// return a middleware used by json server
+const middleware = jsonserver.defaults()
+
+//setup port
+const port =3001
+
+// use in server
+restserver.use(middleware)
+restserver.use(router)
+
+// to run a port
+restserver.listen(port,()=>{
+console.log('rest server listening on port' +port);
+})
